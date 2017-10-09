06e7e065b475d0af9e26ab04ba9c4f76f2c9b495
sarajohansson91 <shjhnssn@gmail.com>
2017-10-09 14:30:14 +0200

    tjolahopp

--- lab7sarasara/R/visualize_airport_delays.R
+++ lab7sarasara/R/visualize_airport_delays.R
@@ -1,8 +1,15 @@
+#' @title Visualize airport arrival delays
+#' @description A function to visualize the mean of airport arrival delays. Each airport is represented 
+#' by their longitude and latitude. 
+#' @return A scatterplot where the size of the points indicates the arrival delay 
+#' @importFrom stats na.omit
+#' @export visualize_airport_delays
+
 visualize_airport_delays <- function(){
   airports <- nycflights13::airports[,c(1,3:4)]
   flights <- nycflights13::flights[,c(14,9)]
   flights <- na.omit(flights)
-  flights <- dplyr::summarise(dplyr::group_by(flights, dest), delay = mean(arr_delay))
+  flights <- dplyr::summarise(dplyr::group_by(flights, flights$dest), delay = mean(flights$arr_delay))
   airport_delays <- dplyr::inner_join(airports, flights, by=c("faa" = "dest"))
   
   ggplot2::ggplot(airport_delays, ggplot2::aes(x=airport_delays$lat, y=airport_delays$lon)) +
